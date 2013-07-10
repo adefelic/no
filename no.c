908dfb1d240ef94b8ff7d94aa7588b951d641c13
adefelic <ad.defelice@live.com>
2013-07-09 21:42:57 -0400

    Create no.c

---
+++ no.c
@@ -0,0 +1,12 @@
+/* no                           */
+/* a program by adam defelice   */
+/* july 9th, 2013               */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  while(1) printf("%s\n", (argc > 1)? "n" : "n");
+
+  return 0;
+}
